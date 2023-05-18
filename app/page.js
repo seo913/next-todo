4c11e9de188e8b395abb7f8bcf1a6b99d0a0fc52
seojaemin <seojaemin@seojaemin-ui-MacBookPro.local>
2023-05-18 11:34:41 +0900

    dynamic rende todo

--- app/page.js
+++ app/page.js
@@ -2,6 +2,8 @@ import { connectDB } from '@/util/database';
 import All from './all/page';
 import Delete from './all/delete';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
   const db = (await connectDB).db('todolist'); //디비 불러오기2 db('forum') 은 내가 만들어논 몽고디비저장소
   let result = await db.collection('todo').find().toArray(); //디비 불러오기3 // 이 줄이 모든 데이터를가져와주세요.
